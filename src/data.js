f9828de5e53aa400b6f734ee10815837d9eb643d
[Razorsujith] <[sujith19961996@gmail.com]>
2023-02-26 14:38:26 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const data = {
+
+    products : [
+
+        {
+            id: '1',
+            name: 'OnePlus Headphones',
+            price: 1500,
+            image: 'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1664409740/Croma%20Assets/Communication/Headphones%20and%20Earphones/Images/257082_0_klwmql.png/mxw_1440,f_auto'
+        },
+
+        {
+            id: '2',
+            name: 'Realme Airpods',
+            price: 2500,
+            image: 'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1674049384/Croma%20Assets/Communication/Headphones%20and%20Earphones/Images/256472_0_hjbqhy.png/mxw_1440,f_auto'
+
+
+
+        },
+        {
+            id: '3',
+            name: 'IPhone-14',
+            price: 100000,
+            image: 'https://www.apple.com/in/iphone-14/images/overview/hero/hero_iphone_14_plus__fra3gmuj0aum_large.jpg'
+
+
+
+        },
+    ],
+};
+
+export default data;
